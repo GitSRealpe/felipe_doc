e815965f48b409bd203745435745db89ce60552e
GitSRealpe <msn-realpe@hotmail.com>
2021-11-08 22:13:35 -0500

    added 4th benchmard and updated other files

--- bechmarking/data_processing/boxplots/time_plot.py
+++ bechmarking/data_processing/boxplots/time_plot.py
@@ -5,7 +5,9 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly
 
-con = sqlite3.connect('../data/bench3/benchmark3.db')
+num=4
+
+con = sqlite3.connect('../data/bench%d/benchmark%d.db' % (num,num))
 
 planners=["RRTConnect", "PRM", "RRT", "LazyPRM", "LBKPIECE", "BKPIECE", "KPIECE", "EST"]
 robots=[(1,"irb_140","orangered"),(3,"kr6","olivedrab"),(5,"ur5","royalblue")]
@@ -16,6 +18,7 @@ for robot in robots:
 
     df=pd.read_sql("SELECT time,request_planner_id FROM runs WHERE experimentid=%d" % robot[0],con)
     print(df)
+    # df["time"].clip(upper=5.05,inplace=True)
 
     fig.add_trace(go.Box(
         y=df["time"],
@@ -26,11 +29,15 @@ for robot in robots:
 
 fig.update_layout(
     yaxis_title='Computation Time',
+    font=dict(
+        family="Courier New, monospace",
+        # size=18,
+    ),
     boxmode='group' # group together boxes of the different traces for each value of x
 )
 
-fig.write_html("tiempo3.html")
+fig.write_html("tiempo"+str(num)+".html")
 
-plotly.io.write_image(fig,"timebox.pdf", format="pdf", width=100, height=100)
+# plotly.io.write_image(fig,"timebox.pdf", format="pdf", width=1000, height=500)
 
 fig.show()
